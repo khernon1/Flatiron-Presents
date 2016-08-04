0c5a98ad67ea9f1a3aae00f81176f8bee7c70f20
Kevin Hernon <khernon1@gmail.com>
2016-08-03 21:36:28 -0400

    we did something

--- notes.md
+++ notes.md
@@ -1,2 +1,7 @@
-var hoverElem = null;
+<!-- var hoverElem = null;
  $('*').hover(function() {hoverElem = this});
+ -->
+
+ Issues
+ 1) jquery not recognized even though it is being added to the html. it works in the console.
+ 2) function was not running on submit when the form was added
--- test.js
+++ test.js
@@ -1,10 +1,26 @@
 // $(document).ready(() => {
 
-  document.onclick = myClickHandler;
-    function myClickHandler() {
-     var clicked = event.target
-     console.log
-     var click_font = $(clicked).css('font-family')
-     console.log(click_font)
-    }
-    
+
+
+  // document.onclick = myClickHandler;
+  //   function myClickHandler() {
+  //     var jq = document.createElement('script');
+  //     jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+  //    var clicked = event.target
+  //    $(clicked).append($(clicked).css('font-family'))
+  //   }
+
+// $('.clear-articles h3').css('font-family', billy_font)
+// $('.artist-name h3').css('font-size', '100px')
+
+var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";document.getElementsByTagName('body')[0].appendChild(script);
+
+$('body').append('<form onsubmit="changeColor()"><input id="font" type="text"><button type="submit"> </button> </form>')
+
+function changeColor() { 
+  event.preventDefault();
+  var font = $('#font').val()
+  // $('#color').on('click', function(){
+     $('h1').css('font-family', font)
+  // })
+}
