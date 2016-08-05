fbb424b0276ed0084a9072f9ab9eeed99bb0c9ab
Dan <dandanberry@gmail.com>
2016-08-04 23:00:56 -0400

    WIP content still deploying on page load  ?

--- extension/background.js
+++ extension/background.js
@@ -5,27 +5,36 @@
 // Called when the user clicks on the browser action (icon)
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-    var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";document.getElementsByTagName('body')[0].appendChild(script);
+  // alert("HELLO")
 
-  document.addEventListener('click', function() {
+  chrome.tabs.executeScript({
+    code: 'content.js'
+  });
+    // var script = document.createElement('script');
+    // script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+    // document.getElementsByTagName('body')[0].appendChild(script);
 
-    debugger
 
-    $('body').append('<form onsubmit="changeColor()"><input id="font" type="text"><button type="submit"> </button> </form>')
 
-    function changeColor() {
-      event.preventDefault();
-      var font = $('#font').val()
-      // $('#color').on('click', function(){
-         $('h1').css('font-family', font)
-      // })
-    }
 
-  })
+
+  //   debugger
+  //
+  //   $('body').append('<form onsubmit="changeColor()"><input id="font" type="text"><button type="submit"> </button> </form>')
+  //
+  //   function changeColor() {
+  //     event.preventDefault();
+  //     var font = $('#font').val()
+  //     // $('#color').on('click', function(){
+  //        $('h1').css('font-family', font)
+  //     // })
+  //   }
+  //
+  // })
   // console.log("Turning blue!");
   // chrome.tabs.executeScript({
   //   code: 'document.body.style.backgroundColor="blue"'
   // });
 });
 
-chrome.browserAction.setIcon({path:"icon-notification.png"});
+// chrome.browserAction.setIcon({path:"icon-notification.png"});
--- talkingpoints.md
+++ talkingpoints.md
@@ -1,12 +1,18 @@
 File Structure:
+  - overall: concise, compare with Mozilla
   - Manifest:
     JSON, what is it
-  - scripts:
+  - injecting (content) scripts:
+    different types of scripts,
+    content script manifest nuances, we don't need no stinking "MATCHES"
+  -
 
 
+challenges in building app:
+getting scripts to load properly
+"persistent": false vs. true
+tabs?
 
-
-
-___ Oddball
-why wont the extension work on another site with an extension already running?
+___ Misc.
+a. why wont the extension work on another site with an extension already running?
 example: momentum (inspirational info extension)
