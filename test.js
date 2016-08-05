913fb5d04569888d1253b988c9c863ac2d21ebbc
Kevin Hernon <khernon1@gmail.com>
2016-08-04 22:13:25 -0400

    works for 175 fonts

--- test.js
+++ test.js
@@ -5,16 +5,11 @@ $(document).click(function(event) {
 
 
 function renderList() {
-  if ($('#__extension__').length !== 1) {    
-    var div = document.createElement("div");
-    div.setAttribute('id', '__extension__')
-    document.body.appendChild(div)
+  if ($('#foo').length !== 1) {    
+    var foo = document.createElement("div");
+    foo.setAttribute('id', 'foo')
+    document.body.appendChild(foo)     
     getAllFonts()
-    // var script = React.createElement('script', {id: "__extension__"})
-    // document.body[0].appendChild(script)
-    // ReactDOM.render(script, document.getElementsByTagName('body'))
-    // $('body').append('<script id="__extension__" </script>')
-    // $('body').append('<form id="_formo_" onclick="getFont()"><input id="font" type="text"><button type="submit"> </button> </form>')    
   }
 }
 
@@ -22,22 +17,30 @@ function renderList() {
 function getAllFonts(){
   $.ajax({
     method: "GET",
-    url: "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCtnKRCf_cI3iQ7WZrU19_GmDeFDmoohCw",
+    url: "https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyCtnKRCf_cI3iQ7WZrU19_GmDeFDmoohCw",
   }).done(function(fonts) {
     fonts_arr = []
     fonts.items.forEach((font)=> {
         fonts_arr.push({category: font.category, family: font.family})
     })
-    addFontsToList(fonts_arr)
+    createList(fonts_arr)
   })
 }
 
-function addFontsToList(arr) {
-  var select_list = React.createElement('select', {id: 'setFont'})
-  ReactDOM.render(select_list, document.getElementById('__extension__'))
+
+function createList(fonts_arr) {
+  root = "http://fonts.googleapis.com/css?family="
+  fonts_arr = fonts_arr.slice(0,175)
+  var fonts_list = fonts_arr.map((font) => {
+    root += font.family + "|"
+    return React.createElement('option', {value: font.family}, font.family)
+  })
+  var select = React.createElement('select', {id: '__extension__'}, fonts_list)
+  ReactDOM.render(select, document.getElementById('foo'))
+  document.getElementById("__extension__").addEventListener('change', getFont)
 }
 
-// function getFont() {
-//   var font = $('#font').val()
-//   $('h1').css('font-family', font)
-// }
+function getFont() {
+  var font = $('#__extension__').val()
+  $('h1').css('font-family', font)
+}
